f843fff4cde49adff16bbee15627b43497f223c1
Mono <Mono@DESKTOP-BR5IUL7>
2021-06-22 15:55:03 -0500

    enfermedades y tratamientos en comentarios

---
+++ assets/js/consulta.js
@@ -0,0 +1,274 @@
+// otitis 
+// moquillo 
+// sarna 
+// parasitosInternos 
+// artrosis 
+// parvovirus 
+// gastritis 
+// conjuntivitis 
+// rabia 
+// leucemiaFelina 
+// panleucopeniaFelina 
+// inmunodeficienciaFelina 
+// peritonitis 
+// cistitis 
+// colaMojada 
+// resfriado 
+// diabetes 
+// acaros 
+// tiña 
+// infeccionUtero 
+// tuberculosis 
+// brucelosis
+// fiebreAftosa
+
+
+// TRATAMIENTO OTITIS
+// En general el tratamiento debe eliminar la causa primaria, controlar las infecciones
+// bacterianas y fúngicas oportunistas y mantener limpio y seco el conducto auditivo
+// con diversas modalidades de tratamiento según la gravedad del cuadro; de sustancias
+// limpiadoras y ceruminolíticas, a corticosteroides tópicos hasta la necesidad de
+// limpiar el conducto bajo anestesia general en casos graves.
+// Una vez realizada la limpieza se tratará el oído dependiendo de qué agente está
+// actuando: acaricidas, antifúngicos o antibióticos. Además de la vía tópica se utilizará
+// la parenteral y/u oral para favorecer la resolución del proceso
+
+
+// TRATAMIENTO MOQUILLO:
+// Actualmente no se conoce ningún tratamiento farmacológico para atacar al virus del
+// moquillo canino y curar el distemper o enfermedad de Carré. Únicamente se lleva a cabo
+// un tratamiento de soporte para ayudar al propio organismo del animal a combatir la
+// infección. En la gran mayoría de los casos es necesaria la hospitalización del perro.
+// En esos casos, el aporte de fluidos por vía intravenosa es fundamental para evitar la
+// deshidratación y suplementar con nutrientes al paciente con anorexia y diarrea. A medida
+// que el perro comience a comer, se puede recurrir a alimentos específicos de fácil
+// digestión y alta energía.
+
+
+// TRATAMIENTO SARNA:
+// El tratamiento para la sarna en perros que el especialista nos recetará puede
+// componerse, principalmente, por un acaricida oral, tópico o inyectable, generalmente
+// con ivermectina, selamectina, moxidectina y milbemicina oxima. Además, nos proporcionará
+// analgésicos, antiinflamatorios, algún champú o polvos para baños con productos calmantes
+// para la piel, con acaricidas y algún medicamento más específico para algún problema
+// cutáneo secundario, como una posible infección por bacterias (bactericida o antibiótico)
+// o por hongos (fungicida).
+
+
+// TRATAMIENTO PARASITOS INTERNOS:
+// El tratamiento contra todos los parásitos internos debe administrarse sin demora una vez
+// confirmado el diagnóstico. En el caso de los gusanos intestinales, el objetivo consiste
+// en eliminar los vermes redondos y las tenias más habituales con una sola dosis de
+// medicamento administrada por vía oral o a través de la piel.
+// Los medicamentos antiparasitarios son altamente eficaces, sencillos de usar y muy seguros,
+// tanto para el propietario como para el animal. Pueden administrarse por vía oral, a menudo
+// en forma de comprimidos que suelen ser de sabor apetitoso, o bien de forma tópica
+// (en la piel), mediante un sistema de aplicación spot-on (es decir, una unción dorsal puntual).
+
+
+// TRATAMIENTO ARTROSIS:
+// -Antiinflamatorios: El tratamiento farmacológico de la artrosis en perros mayores suele
+// incluir agentes antiinflamatorios esteroideos o no esteroideos (AINE), fundamentalmente
+// para aliviar el dolor y la inflamación. Los AINE inhiben la producción de prostaglandinas,
+// las cuales promueven la inflamación.
+// -Esteroides. Los corticosteroides y glucocorticosteroides son un plan de tratamiento muy
+// popular debido a su eficacia para aliviar el dolor y la inflamación, ya que inhiben la
+// producción de ácido araquidónico, lo cual contribuye a detener la inflamación y la
+// producción de prostaglandinas. 
+
+
+// TRATAMIENTO PARVOVIRUS:
+// En cuanto al tratamiento del parvovirus, no existe un tratamiento al 100%, sin embargo,
+// existen pasos a seguir para poder combatirla y tener mejor resultados o en su caso,
+// eliminar totalmente el virus.
+// -Rehidratación: Normalmente se recomienda una administración de suero por vía intravenosa.
+// -Transfusiones de sangre: Esto por la perdida de sangre debido a las diarreas. Una vez
+// estabilizado, se prosigue con cuidados y monitoreo.
+// -Antibióticos.
+
+
+// TRATAMIENTO GASTRITIS:
+// El tratamiento de la gastritis canina suele empezar por retirar al animal la comida durante
+// un plazo de tiempo determinado, que puede ir de las 12 a las 48 horas. En algunos casos el
+// veterinario puede recomendar, también, limitar la cantidad de agua sin quitarla por completo.
+// Además, el médico veterinario recomendará una dieta adecuada que normalmente tiene que darse
+// en raciones pequeñas y frecuentes, hasta que se haya curado la gastritis.
+// Se recetará antibióticos, corticosteroides, antieméticos (para evitar los vómitos) u otros
+// medicamentos que considere oportunos para cada caso. Si la gastritis es causada por un objeto
+// extraño en el estómago, la única solución suele ser la cirugía.
+
+
+// TRATAMIENTO CONJUNTIVITIS:
+// Nunca debemos medicar por decisión propia.  Habitualmente, en el caso de las conjuntivitis
+// que no son secundarias a otras enfermedades sistémicas, el tratamiento se basa en la limpieza
+// del ojo con suero fisiológico y gasas estériles, y la aplicación de antiinflamatorios
+// esteroideos vía oral o inyectada. Habitualmente no se recomienda ningún fármaco de aplicación
+// tópica porque la absorción cutánea es difícil y porque la zona ocular es muy delicada, lo cual
+// lo hace prácticamente imposible. 
+
+
+// TRATAMIENTO RABIA:
+// Desafortunadamente, aún no se conoce una cura para la rabia. Por ello, generalmente los
+// veterinarios recomiendan la eutanasia de los perros infectados, considerando el sufrimiento que
+// la enfermedad les provocará y la posibilidad de contagiar otros individuos.
+// La única forma fiable de prevenir la rabia en perros es aplicar el refuerzo anual de la vacuna
+// contra la rabia en nuestros mejores amigos. Por ello, no olvides mantener siempre actualizado el
+// calendario de vacunación de tu mascota.
+
+
+// TRATAMIENTO LEUCEMIA FELINA:
+// No existe ningún medicamento que pueda eliminar el virus de la leucemia felina o que pueda
+// curar las enfermedades que provoca; de hecho, únicamente puede aplicarse un tratamiento de
+// soporte. A pesar de ello, ese tratamiento puede hacer que el gato mantenga una calidad de vida
+// aceptable durante muchos meses o años.
+// A menudo se precisan largos ciclos de antibióticos para erradicar las infecciones oportunistas.
+// Es conveniente evitar cualquier fuente de estrés, como el que el animal sufre cuando se
+// introducen cambios en su rutina o en su lugar de residencia habitual. Evitar el contacto con
+// fuentes potenciales de otras enfermedades contagiosas como gatos sin vacunar.
+
+
+// TRATAMIENTO PANLEUCOPENIA FELINA:
+// importancia administrar un tratamiento sintomático y de soporte adecuado para el estado del
+// animal. El tratamiento debe administrarse de manera precoz y agresiva.
+// Debido a los vómitos, las diarreas y la fiebre se produce una grave deshidratación con lo que
+// el papel de la fluidoterapia es muy importante. Se puede administrar Ringer Lactato suplementado
+// con KCl  incluso por vía intravenosa, según el grado de deshidratación.
+
+
+// TRATAMIENTO INMUNODEFICIENCIA FELINA:
+// Hasta hoy no hay tratamiento que produzca una remisión de una infección ya establecida.
+// El principal objetivo del tratamiento para un gato infectado por FIV es estabilizar al paciente
+// y mantenerlo con una buena calidad de vida. Aunque no están registrados para su uso en gatos,
+// algunos medicamentos antivirales usados en pacientes con VIH (como la azidotimidina, AZT), han
+// dado  resultados en algunos gatos infectados.
+// El interferón es un compuesto que interfiere con la replicación del virus y ha recibido mucha
+// atención recientemente en el tratamiento de muchas infecciones virales. 
+
+
+
+// TRATAMIENTO PERITONITIS:
+// El tratamiento depende del tipo de peritonitis y en la gravedad que se este presentando.
+// Generalmente, existen tres reglas básicas en el tratamiento de la Peritonitis en perros:
+// estabilizar las constantes fisiológicas del perro, tratar la infección, y localizar y
+// corregir las causas.
+// Existen casos en los que ha sido necesario intervenciones quirúrgicas, debido al liquido
+// acumulado, por lo que es necesario un drenaje abdominal. En el caso de que tu perro lo
+// necesite, no olvides que es de suma importancia los cuidados postoperatorios y el apoyo
+// nutricional.
+
+
+// TRATAMIENTO CISTITIS:
+// El tratamiento para la cistitis canina siempre suele basarse en la administración de
+// antibióticos que solo el veterinario puede recetar tras la prueba de sensibilidad.
+// Recuerda que no debes interrumpir el tratamiento estipulado por el especialista a menos
+// que él mismo así lo solicite.
+// Por otro lado, es imprescindible que te asegures de que tu perro ingiere la cantidad de
+// agua que necesita, puesto que este hecho favorecerá considerablemente su recuperación.
+// En caso de padecer piedras en la vejiga, tumor o diabetes, el tratamiento irá dirigido
+// tanto a curar la cistitis como a asistir la patología que sufra.
+
+
+
+// TRATAMIENTO COLA MOJADA:
+// Para intentar un tratamiento es importante mantener al enfermo calentito y limpio, con el
+// menor estrés posible. Los líquidos que se pierden por la diarrea deben ser repuestos por
+// boca con una solución con electrolitos pediátrica o con inyecciones de Ringer lactato y hay
+// que suspender todo alimento sólido durante al menos 24 horas. Generalmente se prescribe un
+// antibiótico para gérmenes gram negativos, como neomicina o enrofloxacina y se administran
+// por boca en forma de gotas una o dos veces al día. Los tratamientos de sostén inespecíficos
+// que se agregan son Kaolina, 2 a 3 gotas 3 veces al día, vitamina B (como levadura de cerveza)
+// o un multivitamínico.
+
+
+// TRATAMIENTO RESFRIADO:
+// Medicamentos como Vicks VapoRub se puede utilizar, colocarlo fuera del alcance del hámster
+// enfermo, pero lo suficientemente cerca para que lo inhale, con lo cual lo ayudará a respirar
+// mejor y puede ayudarle a aliviar algo la congestión.
+// Además, un viejo remedio Inglés es la leche tibia con agua y con una cucharadita de miel, pero
+// si tu hámster no mejora dentro de dos días del tratamiento, debes buscar ayuda veterinaria.
+// La pérdida de peso, estremecimientos, castañeteo y conjuntivitis pueden indicar Pasteurella
+// pneumotropica, una infección de los pulmones. Una vez más, el tratamiento es con antibióticos
+// prescrito por el veterinario.
+
+
+// TRATAMIENTO DIABETES:
+// El tratamiento de la diabetes canina se basa en tres puntos clave:
+// -Dieta: elige alimentos ricos en fibra, que consigan retrasar la absorción de glucosa en el
+// intestino. Recuerda repartir la ración diaria en varias tomas, adaptándola a la administración
+// de insulina.
+// -Ejercicio: la actividad deportiva en el perro resulta beneficiosa para controlar la obesidad
+// y mantener el peso ideal. En cualquier caso, evita ejercicios bruscos o extenuantes.
+// -Medicamentos:
+//   >Hipoglucemiantes orales: >Insulina: cuando no pueden sintetizarla, debes administrársela tú.
+
+
+// TRATAMIENTO ACAROS:
+// En general, todos los ácaros en perros se tratan con insecticidas. Para perros adultos se
+// recomiendan baños de Amitraz, con la frecuencia que establezca el veterinario (suelen ser
+// cada 2 semanas). Otro tratamiento de elección es la Ivermectina (insecticida sistémico).
+// En el caso de cachorros, debido a que la sarna más frecuente es la cheyleteliosis, se recomienda
+// el cepillado del animal para eliminar la caspa, aplicar un insecticida apto para cachorros y
+// también aplicar insecticida en los lugares que frecuente el animal del hogar, así como lavar con
+// programa de agua caliente la cama y otros lugares de reposo. En el caso de la otoacariosis se
+// recomienda unas gotas óticas con insecticida incorporado y realizar un tratamiento con spray.
+
+
+// TRATAMIENTO TIÑA:
+// Es cierto que muchas veces si se diagnostica la tiña en fases principales, sólo con mejorar el
+// sistema inmunológico del animal afectado, la tiña remite en pocos meses. Por lo tanto, en estos
+// casos además de ayudar a la mejoría del sistema de defensas de nuestro compañero, el objetivo del
+// tratamiento es más bien sintomático y para evitar la propagación del hongo.
+// Principalmente es suficiente con un tratamiento fungicida tópico en forma de pomada, polvos o
+// loción. En el caso de utilizar una loción o unos polvos, podemos ayudarnos con un cepillo para
+// poder extender bien el producto a la vez que ayudamos a eliminar esporas de los hongos
+// acumuladas en el pelaje.
+
+
+// TRATAMIENTO INFECCION UTERO:
+// La práctica totalidad de los casos de piométra o infección uterina de la perra precisan de una
+// intervención quirúrgica llamada ovariohisterectomía, que consiste en la extirpación de los ovarios
+// y el útero canino. Hay casos en los que antes de la cirugía es necesario tratar a la perra con
+// antibióticos para reducir la infección.
+// Antes de operar a la perra hay que estabilizar su situación en caso de que el funcionamiento renal
+// esté alterado y la sangre intoxicada por la infección, pero la intervención quirúrgica no puede
+// tardar, porque la perra puede morir si no se actúa con rapidez.
+
+
+// PREVENCION TUBERCULOSIS:
+// • Rara vez se trata a los animales infectados, porque además del peligro de contagio, resulta
+// muy caro y prolongado, y porque el gran objetivo último es erradicar la enfermedad.
+// • Los animales infectados se sacrifican.
+// • La pasteurización de la leche de animales infectados a una temperatura suficiente para matar
+// a las bacterias impide que la enfermedad se propague al hombre.
+// • En el hombre, la enfermedad se trata con antimicrobianos
+
+
+// PREVENCION BRUCELOSIS:
+// De esta forma, a día de hoy no existe un tratamiento para la brucelosis en las vacas, por lo que
+// todas las que enferman son sacrificadas. Esto se explica por el uso de las vacas como fuente de
+// alimento, contando tanto la leche como la carne, lo cual hace muy peligroso que las personas
+// puedan consumir carne o leche proveniente de vacas contagiadas. Es por esta razón, debido a que
+// la brucelosis es una zoonosis, por lo que, al no existir un tratamiento o medicamento que asegure
+// totalmente la desaparición del patógeno, se aboga por eutanasiar al animal.
+
+
+// PREVENCION FIEBRE AFTOSA:
+// La fiebre aftosa debe notificarse a la Organización Mundial de Sanidad Animal (OIE). Los requisitos
+// de notificación de las enfermedad a las naciones miembro de la OIE y las pautas de importación /
+// exportación pueden encontrarse en el Código Sanitario para los animales terrestres de la OIE
+// [http://www.oie.int/eng/normes/mcode/A_summry.htm]. Los veterinarios que encuentren un caso de
+// Fiebre Aftosa deben seguir las pautas nacionales y/o locales Fiebre Aftosa Última Actualización:
+// Septiembre del 2007 © 2010 page 6 of 6 para la notificación y las pruebas de diagnóstico de la
+// enfermedad.
+
+
+
+
+
+
+
+
+
+
+
+
